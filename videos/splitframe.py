c1ffd9c1b113900c477e045705545a5688d80419
Duc Minh Tran <ductm22@fsoft.com.vn>
2019-01-09 18:38:06 +0700

    Add write functions

--- videos/splitframe.py
+++ videos/splitframe.py
@@ -2,18 +2,14 @@ import cv2
 import argparse
 import os
 
-DIR = 'videos/'
-
 ap = argparse.ArgumentParser()
 ap.add_argument('-v', '--video', type=str, help='video input', required=True)
 ap.add_argument('-f', type=int, help='amount of frame to skip', required=True)
 args = vars(ap.parse_args())
 
-filename = args.get('video')[7:-4]
-print(filename)
-# os.mkdir(filename)
+filename = args.get('video')[:-4]
 
-vidcap = cv2.VideoCapture(filename)
+vidcap = cv2.VideoCapture(args.get('video'))
 success,image = vidcap.read()
 count = 0
 
@@ -26,12 +22,17 @@ while success: # check success here might break your program
   #check success here
   if not success:
     break
-
+  
   # on every numFrameToSave
   if (count % numFrameToSave ==0):
-    if os.path.isdir(filename):
-      path = os.path.join(DIR, '{}_%d.jpg'.format(filename)%count)
+    if os.path.isdir('images'):
+      path = os.path.join('images', '{}_%d.jpg'.format(filename)%count)
       cv2.imwrite(path, image)
+    else:
+      os.mkdir('images')
+      path = os.path.join('images', '{}_%d.jpg'.format(filename)%count)
+      cv2.imwrite(path, image)
+          
   if cv2.waitKey(10) == 27:
       break
   count += 1
